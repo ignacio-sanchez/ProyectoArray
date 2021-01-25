8afd7d6daa68496dd68d3ceb5c10ff4fde54ae66
Ignacio <ignacio.sanchez@campusfp.es>
2021-01-25 10:58:15 +0100

    SNAPSHOT 3 Main2.java

---
+++ src/misrc/Main2.java
@@ -0,0 +1,14 @@
+
+
+package misrc;
+
+public class Main2 {
+    
+    public static void main(String[] args) {
+        char[] caracteres = {'H', 'O', 'L', 'A'};
+        String cadena = String.valueOf(caracteres);
+        
+        System.out.println(cadena);
+    }
+    
+}
