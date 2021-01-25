490cee6289f1eed5ac3dd17c4496524342faf4f8
Ignacio <ignacio.sanchez@campusfp.es>
2021-01-25 10:00:12 +0100

    1 SNAPSHOT Main1.java

---
+++ src/misrc/Main1.java
@@ -0,0 +1,18 @@
+
+package misrc;
+
+
+public class Main1 {
+
+
+    public static void main(String[] args) {
+    // DECLARACIÓN DEL ARRAY   
+        String[] nombre = {"Luis", "Miguel", "Carlos"};
+    
+    // RECORRER EL ARRAY
+        for(int i=0; i<nombre.length; i++) {
+            System.out.println(nombre[i]);
+        }
+    
+    }
+}
